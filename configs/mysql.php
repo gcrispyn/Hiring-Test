94e87bcb817ba700af7f04ae9253f21c85512601
CRISPYN <gael.crispyn@gmail.com>
2016-04-04 19:27:18 +0000

    Change name of database in mysql config

--- configs/mysql.php
+++ configs/mysql.php
@@ -5,7 +5,7 @@ return [
     'db.options' => [
         'driver' => 'pdo_mysql',
         'dbhost' => '127.0.0.1',
-        'dbname' => 'hiring-test',
+        'dbname' => 'hiringtest',
         'user' => 'root',
         'password' => '',
     ]
